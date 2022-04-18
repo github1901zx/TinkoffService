32f8d5627ec2d75bfe425ab4c8e3956213f8d27a
Alexander <h0c0kutaiiok@gmail.com>
2022-04-18 19:31:45 +0300

    fist commit

---
+++ TinkoffService/src/test/java/com/alexanderpopov/tinkoffservice/TinkoffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexanderpopov.tinkoffservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
